24e37aa91a6b03548ef515883b9ba856e125b151
RobbieJ117 <RobbieDavis777@gmail.com>
2017-11-13 06:33:11 -0700

    Getting test results for ES and GA

--- ES_exp.py
+++ ES_exp.py
@@ -2,7 +2,7 @@ import Experimenter
 import MLP
 import DataHandler
 
-dh = DataHandler.DataHandler("Data/news.csv", 1, token=",")
-network = MLP.MLP("ES_news", dh.num_features,7, 1)
+dh = DataHandler.DataHandler("Data/MusicPlus.txt", 1, token=",")
+network = MLP.MLP("ES_MusicPlus", dh.num_features,7, 1)
 exp_1 = Experimenter.Experimenter(dh, network, 3, maxgen=150)
 exp_1.five_by_two()
