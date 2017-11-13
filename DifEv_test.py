3a768762e904fea3fea1168db2835372950174ca
Bryan Downs <xerobasix@gmail.com>
2017-11-12 19:25:36 -0700

    add energy graph

--- DifEv_test.py
+++ DifEv_test.py
@@ -2,7 +2,7 @@ import Experimenter
 import MLP
 import DataHandler
 
-dh = DataHandler.DataHandler("Data/3d_test.txt", 1, token=",")
-network = MLP.MLP("dif_ev_3dTest", dh.num_features,7, 1)
+dh = DataHandler.DataHandler("Data/Music.txt", 1, token="")
+network = MLP.MLP("dif_ev_music", dh.num_features,7, 1)
 exp_1 = Experimenter.Experimenter(dh, network, 2)
 exp_1.five_by_two()
