37b42bf010ee1fa751e2882ed34f8b79744f7fe0
RobbieJ117 <RobbieDavis777@gmail.com>
2017-11-13 07:23:40 -0700

    Some test results added

--- GA_exp.py
+++ GA_exp.py
@@ -2,7 +2,7 @@ import Experimenter
 import MLP
 import DataHandler
 
-dh = DataHandler.DataHandler("Data/MusicPlus.txt", 1, token=",")
-network = MLP.MLP("GA_MusicPlus", dh.num_features,7, 1)
+dh = DataHandler.DataHandler("Data/News.csv", 1, token=",")
+network = MLP.MLP("GA_News", dh.num_features,7, 1)
 exp_1 = Experimenter.Experimenter(dh, network, 4, maxgen=150)
 exp_1.five_by_two()
